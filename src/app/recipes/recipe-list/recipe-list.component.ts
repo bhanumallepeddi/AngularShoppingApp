19f93afeda8b38e3386a8da0d4a8ec231fcb9aba
bhanumallepeddi <bhanumallepeddi@gmail.com>
2019-02-12 07:31:01 -0600

    Initial UI Commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test recipe',
+     'https://diethood.com/wp-content/uploads/2018/01/Easy-Baked-Chicken-9.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit() {
