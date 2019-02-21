74fd83c1aea814564de144ba096d179faf761df5
bhanumallepeddi <bhanumallepeddi@gmail.com>
2019-02-21 14:22:30 -0600

    Building and using a dropdown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,8 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Test Recipe', 'This is simply a test recipe',
      'https://diethood.com/wp-content/uploads/2018/01/Easy-Baked-Chicken-9.jpg'),
      new Recipe('Thai cuisine', 'Delicious Thai food',
-     'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/slideshows/best_and_worst_thai_dishes_slideshow/1800x1200_slideshow_best_and_worst_thai_dishes_for_your_health.jpg')
+     `https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/
+     article_thumbnails/slideshows/best_and_worst_thai_dishes_slideshow/1800x1200_slideshow_best_and_worst_thai_dishes_for_your_health.jpg`)
 
   ];
 
